751d7bcf13983faf159aef224d45ad11473bc3e3
Gabriel Schulte <gabrielmdu@gmail.com>
2020-08-23 10:38:57 -0300

    Filled the skills session

--- src/components/content/Skills.js
+++ src/components/content/Skills.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as ProgLogo } from '../../assets/img/prog.svg';
+import { ReactComponent as ToolsLogo } from '../../assets/img/tools.svg';
+import { ReactComponent as LanguagesLogo } from '../../assets/img/languages.svg';
+import './Skills.scss';
 
 const Skills = ({ classes }) => {
+  const [t] = useTranslation();
+
+  const skillSessions = [{
+    title: t('sessions.skills.prog'),
+    skills: ['PHP', 'JavaScript', 'CSS/SCSS', 'HTML', 'Python', 'C++', 'SQL'],
+    image: ProgLogo
+  }, {
+    title: t('sessions.skills.tools'),
+    skills: [
+      'VS Code',
+      'GIT',
+      'Jira',
+      'Docker',
+      'Postman',
+      'MySQL Workbench',
+      'Visual Studio',
+      'SQL Manager',
+      'SVN',
+      'C++Builder',
+      'Redmine',
+      'GIMP',
+      'Inkscape'],
+      image: ToolsLogo
+  }, {
+    title: t('sessions.skills.languages.title'),
+    skills: t('sessions.skills.languages.list', { returnObjects: true }),
+    image: LanguagesLogo
+  }];
+
   return (
     <div className={classes}>
-      <div>Skills</div>
+      {skillSessions.map((s, i) =>
+        <div key={i} className="skill-session mb-5">
+          <div className="skill-title flex items-center px-2 py-1">
+            <s.image className="inline-block mr-1 fill-current text-gray-200" />{s.title}
+          </div>
+          <div className="flex flex-wrap pt-2 pl-2">
+            {s.skills.map((p, i) =>
+              <div key={i} className="pb-2 pr-2">
+                <span className="skill rounded-full text-sm px-2 py-1">{p}</span>
+              </div>)}
+          </div>
+        </div>)}
     </div>
   );
 };
