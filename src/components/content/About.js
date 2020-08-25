1d58791fa8595a282cca792c17b594ebb5bed4ce
Gabriel Schulte <gabrielmdu@gmail.com>
2020-08-24 23:36:30 -0300

    Filled the about session

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -1,28 +1,27 @@
-import React from "react";
+import React from 'react';
+import gsPic from '../../assets/img/gs.jpg';
+import { useTranslation } from 'react-i18next';
+import './About.scss';
 
 const About = ({ classes }) => {
+  const [t] = useTranslation();
+
+  const paragraphs = t('sessions.about.paragraphs', { returnObjects: true });
+
   return (
     <div className={classes}>
-      <div>
-        <img
-          className="float-none sm:float-left w-64 sm:w-32 mx-auto"
-          src="https://www.vettedpetcare.com/vetted-blog/wp-content/uploads/2018/02/is-your-dog-fat-shutterstock_618287804.jpg"
-          alt=""
-        />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget
-          nunc scelerisque viverra. Elementum nibh tellus molestie nunc non
-          blandit massa. Ultrices sagittis orci a scelerisque purus. Lorem mollis
-          aliquam ut porttitor leo a. Iaculis urna id volutpat lacus laoreet non
-          curabitur. Risus pretium quam vulputate dignissim. Posuere morbi leo
-          urna molestie at. Cras pulvinar mattis nunc sed. Curabitur gravida arcu
-          ac tortor dignissim convallis aenean et tortor. Vel quam elementum
-          pulvinar etiam non quam. Viverra vitae congue eu consequat ac felis
-          donec et. Dolor magna eget est lorem. Scelerisque eu ultrices vitae
-          auctor eu augue. Scelerisque fermentum dui faucibus in ornare quam.
-          Fringilla phasellus faucibus scelerisque eleifend donec pretium
-          vulputate sapien. Proin fermentum leo vel orci porta non.
+      <div className="about flex flex-col sm:flex-row w-full">
+        <div className="pic-wrapper flex-grow-0 mb-4 sm:mb-0 sm:mr-4">
+          <img
+            className="rounded-full border-2 p-1 border-gray-600 mx-auto w-48 sm:w-auto"
+            src={gsPic}
+            alt="Gabriel Schulte"
+          />
         </div>
+        <div className="flex-1 text-sm">
+          {paragraphs.map((p, i) => <p key={i} dangerouslySetInnerHTML={{ __html: p }} />)}
+        </div>
+      </div>
     </div>
   );
 };
